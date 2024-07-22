6f1a5ea87086b605e6e84e1b7b4c13846a30a7d1
neilosu <chouyuh@oregonstate.edu>
2024-07-22 23:35:22 +0800

    chore: add progress 2

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'media_handling',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
--- chat_app/chat_app/urls.py
+++ chat_app/chat_app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('media/', include('media_handling.urls')),
+    path('', include('frontend.urls')),
 ]
