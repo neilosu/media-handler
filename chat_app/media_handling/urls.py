dbed103e9869bde08e15330e3914525c16ae6e57
neilosu <chouyuh@oregonstate.edu>
2024-08-05 04:09:32 +0800

    chore: update image retrieve

--- chat_app/media_handling/urls.py
+++ chat_app/media_handling/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ImageUploadView
+from .views import ImageUploadView, ImageView
 
 urlpatterns = [
     path('upload/', ImageUploadView.as_view(), name='image-upload'),
+    path('image/<str:image_name>/', ImageView.as_view(), name='image-view'),
 ]
--- chat_app/media_handling/views.py
+++ chat_app/media_handling/views.py
@@ -11,3 +11,14 @@ class ImageUploadView(APIView):
             serializer.save()
             return Response("Upload successfully!", status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+from django.http import HttpResponse
+from .models import ImageUpload
+
+class ImageView(APIView):
+    def get(self, request, image_name):
+        try:
+            image = ImageUpload.objects.get(image_name=image_name)
+            return HttpResponse(image.image, content_type='image/png')
+        except ImageUpload.DoesNotExist:
+            return HttpResponse(status=404)
