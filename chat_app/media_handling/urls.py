cdd7454a52a6275e27cc5a0b326fae738a8974e0
neilosu <chouyuh@oregonstate.edu>
2024-08-05 04:46:45 +0800

    chore: add thumbnail support

--- chat_app/media_handling/urls.py
+++ chat_app/media_handling/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ImageUploadView, ImageView
+from .views import ImageUploadView, ImageView, ThumbnailView
 
 urlpatterns = [
     path('upload/', ImageUploadView.as_view(), name='image-upload'),
     path('image/<str:image_name>/', ImageView.as_view(), name='image-view'),
+    path('thumbnail/<str:image_name>/', ThumbnailView.as_view(), name='thumbnail-view'),
 ]
--- chat_app/media_handling/views.py
+++ chat_app/media_handling/views.py
@@ -3,13 +3,19 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import ImageUpload
 from .serializers import ImageUploadSerializer
+from django.http import HttpResponse
+from PIL import Image
+from io import BytesIO
+from django.http import JsonResponse
+
 
 class ImageUploadView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = ImageUploadSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            return Response("Upload successfully!", status=status.HTTP_201_CREATED)
+            response_data = {'image_name': request.FILES['image'].name}
+            return JsonResponse(response_data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 from django.http import HttpResponse
@@ -18,7 +24,24 @@ from .models import ImageUpload
 class ImageView(APIView):
     def get(self, request, image_name):
         try:
-            image = ImageUpload.objects.get(image_name=image_name)
-            return HttpResponse(image.image, content_type='image/png')
+            image_record = ImageUpload.objects.get(image_name=image_name)
+            return HttpResponse(image_record.image, content_type='image/png')
+        except ImageUpload.DoesNotExist:
+            return HttpResponse(status=404)
+
+class ThumbnailView(APIView):
+    def get(self, request, image_name):
+        try:
+            image_record = ImageUpload.objects.get(image_name=image_name)
+            image = Image.open(BytesIO(image_record.image))
+            
+            # Create thumbnail
+            thumbnail_size = (100, 100)
+            image.thumbnail(thumbnail_size)
+            thumb_io = BytesIO()
+            image.save(thumb_io, format='PNG')
+            thumb_io.seek(0)
+
+            return HttpResponse(thumb_io.read(), content_type='image/png')
         except ImageUpload.DoesNotExist:
             return HttpResponse(status=404)
